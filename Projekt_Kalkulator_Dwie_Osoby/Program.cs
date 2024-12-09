e61413d76b08430d41ef936032ee9844c0a1f044
4TP Mateusz Iwan <4TP Mateusz Iwan@S27K106>
2024-12-09 14:39:56 +0100

    Przetestowano metody dodane ze scalania galezi MeFiu i Tomusz

--- Projekt_Kalkulator_Dwie_Osoby/Program.cs
+++ Projekt_Kalkulator_Dwie_Osoby/Program.cs
@@ -10,6 +10,11 @@ namespace Projekt_Kalkulator_Dwie_Osoby
     {
         static void Main(string[] args)
         {
+            Kalkulator k = new Kalkulator();
+            Console.WriteLine(k.Odejmowanie(5, 7));
+            Console.WriteLine(k.Dodaj(4, 440));
+            Console.WriteLine(k.Dzielenie(400,50));
+            Console.WriteLine(k.Mnozenie(-8,-92));
         }
     }
 }
