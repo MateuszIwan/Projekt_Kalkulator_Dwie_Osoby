6d0a9d3e05694b4215054597233e78715187c888
4TP_JasicaT <4TP_JasicaT@S27101>
2024-12-09 14:41:21 +0100

    dodanie bonifacego

--- Projekt_Kalkulator_Dwie_Osoby/Kalkulator.cs
+++ Projekt_Kalkulator_Dwie_Osoby/Kalkulator.cs
@@ -17,4 +17,21 @@ namespace Projekt_Kalkulator_Dwie_Osoby
             return a * b;
         }
     }
+    public static int Bonifacy(int n)
+    {
+        if (n == 0)
+            return 0;
+        if (n == 1 || n == 2)
+            return 1;
+
+        int l1 = 0;
+        int l2 = 1;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            l2 = l2 + l1;
+            l1 = l2 - l1;
+        }
+        return l2;
+    }
 }
