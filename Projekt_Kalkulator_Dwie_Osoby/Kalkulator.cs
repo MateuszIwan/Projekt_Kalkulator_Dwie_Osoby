a52b0d192fe8115d48ece0ca74137c8eb3662239
4TP Mateusz Iwan <4TP Mateusz Iwan@S27K106>
2024-12-09 14:06:55 +0100

    Dodano metodę Odejmowanie(int a, int b) odejmujące liczbę b od liczby a

--- Projekt_Kalkulator_Dwie_Osoby/Kalkulator.cs
+++ Projekt_Kalkulator_Dwie_Osoby/Kalkulator.cs
@@ -8,6 +8,9 @@ namespace Projekt_Kalkulator_Dwie_Osoby
 {
     internal class Kalkulator
     {
-
+        public int Odejmowanie(int a, int b) //odejmuje liczbe b od liczby a
+        {
+            return a - b;
+        }
     }
 }
